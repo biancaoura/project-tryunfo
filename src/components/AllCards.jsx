b8e4bda47077acc8c5a116b8717263a570797efd
Bianca Oura <biancaoura@gmail.com>
2022-07-25 21:02:03 -0300

    Formata

--- src/components/AllCards.jsx
+++ src/components/AllCards.jsx
@@ -8,22 +8,13 @@ class AllCards extends Component {
     return (
       <div>
         {
-          savedCards.map(({ cardName, cardDescription, cardAttr1, cardAttr2, cardAttr3, cardImage, cardRare, cardTrunfo }) => (
+          savedCards.map((cardInfo, index) => (
             <Card
-              key={ cardName }
-              cardName={ cardName }
-              cardDescription={ cardDescription }
-              cardAttr1={ cardAttr1 }
-              cardAttr2={ cardAttr2 }
-              cardAttr3={ cardAttr3 }
-              cardImage={ cardImage }
-              cardRare={ cardRare }
-              cardTrunfo={ cardTrunfo }
+              { ...cardInfo }
+              key={ index }
             />
           ))
         }
-        {/* renderizar todas as cartas que estão no savedCards */}
-        {/* atualizar lista sempre que clicar no botão Salvar */}
       </div>
     );
   }
